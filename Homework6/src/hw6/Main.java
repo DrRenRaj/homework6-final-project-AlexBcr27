f317acb1788d0f315a2b6052099fefd65ad664fb
AlexBcr27 <anbramhall@gmail.com>
2025-05-05 12:05:23 -0600

    got some of the library class done and started on the main

--- Homework6/src/hw6/Main.java
+++ Homework6/src/hw6/Main.java
@@ -1,21 +1,21 @@
 package hw6;
+import java.util.*;
 
-//Add other classes here or separate java files in same package
 class Book{
-	String titl;
+	String title;
 	String author;
 	String isbn;
 	Boolean isAvailable;
 	
 	public Book(String t, String a, String bn, Boolean isa){
-		titl = t;
+		title = t;
 		author = a;
 		isbn = bn;
 		isAvailable = isa;
 	}
 
 	public String GetTitle(){
-		return titl;
+		return title;
 	}
 
 	public String GetAuthor(){
@@ -35,7 +35,37 @@ class Book{
 	}	
 }
 
-public Library {
+class Library {
+	ArrayList<Book> book;
+
+	/*
+	public void addBook(Book book){
+		book.add(book);
+	}
+
+	public void removeBook(String isbn){
+		book.remove(isbn);
+	}
+
+	public displayAllBooks(){
+	
+	}
+
+	public String SearchByTitle(String title){
+		return book.
+	}
+
+	public String searchByAuthor(String author){
+		return 
+	}
+
+	public String checkOutBook(String isbn){
+
+	}
+
+	public String returnBook(String isbn){
+
+	}*/
 
 }
 
@@ -55,6 +85,14 @@ public class Main {
 		   8. Exit
 
 		 */
+		int menu = 0;
+		Scanner scan = new Scanner(System.in);
+		do{
+			System.out.println("please pick a choice from this list:\n1. to add a book\n2. to remove a book");
+			System.out.println("3. to display all books\n4. to search by title\n 5. to search by author");
+			System.out.println("6. to check out a book\n7. to return a book\n8. to exit");
+		}while(menu != 8);
+
 		
 	}
 
